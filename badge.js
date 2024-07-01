bef5d11e21b15f9680f411433b82865eaccf4219
Lee G <34814972-lee82@users.noreply.replit.com>
2024-07-01 16:03:18 +0000

    initialize

--- badge.js
+++ badge.js
@@ -1,30 +1,25 @@
 /**
- * This script adds a "DAD YEARS" badge to your page when seen in full-browser view
+ * This script adds a "DAD YEARS" badge to your page
  */
 
 (function replitBadge(
-  theme = 'dark',
-  position = 'bottom-left',
+  theme = "dark",
+  position = "bottom-left",
   caption = null,
 ) {
-  // Suppress badge in ReplView
-  if (window.location.hostname.split('.')[1] === 'id') {
-    return;
-  }
-
   // Load Baloo font from Google Fonts
-  const fontLink = document.createElement('link');
-  fontLink.href = 'https://fonts.googleapis.com/css2?family=Baloo&display=swap';
-  fontLink.rel = 'stylesheet';
+  const fontLink = document.createElement("link");
+  fontLink.href = "https://fonts.googleapis.com/css2?family=Baloo&display=swap";
+  fontLink.rel = "stylesheet";
   document.head.appendChild(fontLink);
 
   // define positions
-  const offset = '1.5rem';
+  const offset = "1.5rem";
   const validPositions = {
-    'top-left': { top: offset, left: offset },
-    'top-right': { top: offset, right: offset },
-    'bottom-left': { bottom: offset, left: offset },
-    'bottom-right': { bottom: offset, right: offset },
+    "top-left": { top: offset, left: offset },
+    "top-right": { top: offset, right: offset },
+    "bottom-left": { bottom: offset, left: offset },
+    "bottom-right": { bottom: offset, right: offset },
   };
 
   // ensure positions are valid
@@ -32,25 +27,25 @@
     console.warn(
       `${position} is not a valid position, defaulting to bottom-left`,
     );
-    position = 'bottom-left';
+    position = "bottom-left";
   }
 
   // create link & styles
-  const badgeAnchor = document.createElement('a');
+  const badgeAnchor = document.createElement("a");
   Object.assign(badgeAnchor, {
-    target: '_blank',
-    href: 'https://dadyears.art',
+    target: "_blank",
+    href: "https://dadyears.art",
   });
 
   // create badge text & styles
-  const badgeText = document.createElement('div');
-  badgeText.textContent = 'DAD YEARS';
-  badgeText.id = 'replitBadge';
+  const badgeText = document.createElement("div");
+  badgeText.textContent = "DAD YEARS";
+  badgeText.id = "replitBadge";
   Object.assign(badgeText.style, validPositions[position]);
 
   // inject styles
   document.head.insertAdjacentHTML(
-    'beforeend',
+    "beforeend",
     `
     <style>
       #replitBadge {
@@ -58,8 +53,8 @@
         cursor: pointer;
         z-index: 100;
         transition: transform 100ms ease-in-out;
-        background-color: ${theme === 'dark' ? '#333' : '#fff'};
-        color: ${theme === 'dark' ? '#fff' : '#000'};
+        background-color: ${theme === "dark" ? "#333" : "#fff"};
+        color: ${theme === "dark" ? "#fff" : "#000"};
         padding: 0.5rem 1rem;
         border-radius: 0.5rem;
         box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
@@ -79,7 +74,7 @@
   badgeAnchor.appendChild(badgeText);
   document.body.appendChild(badgeAnchor);
 })(
-  document.currentScript.getAttribute('theme'),
-  document.currentScript.getAttribute('position'),
-  document.currentScript.getAttribute('caption'),
+  document.currentScript.getAttribute("theme"),
+  document.currentScript.getAttribute("position"),
+  document.currentScript.getAttribute("caption"),
 );
